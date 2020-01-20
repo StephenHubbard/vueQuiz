570f764c2a60be841bb24e45506576c902a8b0b6
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-01-20 14:09:22 -0700

    centered header for deployment

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-nav tabs>
+    <b-nav tabs style="display: flex; justify-content: center; align-items: center;">
       <b-nav-item disabled><b>Vue Quiz App</b></b-nav-item>
       <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
     </b-nav>
