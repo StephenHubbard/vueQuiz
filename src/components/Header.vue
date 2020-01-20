e8ec5e4d52e35859559e8839888b633249783bd2
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-01-19 18:25:36 -0700

    text change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-nav tabs>
-      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled><b>Vue Quiz App</b></b-nav-item>
       <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
     </b-nav>
   </div>
